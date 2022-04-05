e52d3fc720695749eb075cc2a59e8b2cc65239d2
DespoTheAnimal <thedanishmagnus@gmail.com>
2022-04-05 08:58:28 +0200

    Updated the timer
    Oracles will now de-spawn after 3 seconds of being alive :))

--- Sound Manager Assignment/Assets/Scripts/PlaySoundOnStart.cs
+++ Sound Manager Assignment/Assets/Scripts/PlaySoundOnStart.cs
@@ -5,36 +5,12 @@ using UnityEngine.UI;
 
 public class PlaySoundOnStart : MonoBehaviour
 {
-   /* [SerializeField]
-    private GameObject _oracle1;
-    [SerializeField]
-    private GameObject _oracle2;
-    [SerializeField]
-    private GameObject _oracle3;*/
-
     [SerializeField]
     private AudioClip _clip;
     // Start is called before the first frame update
     void Start()
     {
         SoundManager.Instance.PlaySound(_clip);
+        Destroy(gameObject, 3f);
     }
-   
-    
-    /* private void OnClick()
-    {
-        _randomNumber = Random.Range(_minNum, _maxNum);
-
-        switch (_randomNumber)
-        {
-            case 0: Instantiate(_oracle1, (Vector3.up), Quaternion.identity); break;
-
-            case 1: Instantiate(_oracle2, (Vector3.zero), Quaternion.identity); break;
-
-            case 2: Instantiate(_oracle3, (Vector3.up), Quaternion.identity); break;
-        }
-    }*/
-
-
-
 }
